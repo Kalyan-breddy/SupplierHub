103462c0c605f12cfbca98a89a821c201315db10
A <2478735@cognizant.com>
2026-03-10 10:19:15 +0530

    Created Requisition Model

---
+++ SupplierHub/Models/RequisitionModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Numerics;
+
+namespace SupplierHub.Models
+{
+	public class Requisition
+	{
+		[Key]
+		public long PRID { get; set; }
+
+		[Required]
+		public long RequesterID { get; set; }
+
+		[Required]
+		public long OrgID { get; set; }
+
+		[StringLength(100)]
+		public string CostCenter { get; set; }
+
+		public string Justification { get; set; }
+
+		[Required]
+		public DateTimeOffset RequestedDate { get; set; } = DateTimeOffset.Now;
+
+		public DateTimeOffset? NeededByDate { get; set; }
+
+		[Required]
+		[StringLength(20)]
+		public string Status { get; set; } = "Draft"; // Default as per your schema
+
+		
+	}
+}
